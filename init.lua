cd9827d0444f1875fb90bc833fd4508eb7a28ad3
borkydev <175458672+borkydev@users.noreply.github.com>
2024-07-22 21:54:38 +0200

    removed showing of cursor line on the status bar

--- init.lua
+++ init.lua
@@ -52,9 +52,6 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣', lead = '·', exte
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
