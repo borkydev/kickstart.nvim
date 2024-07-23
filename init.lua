7e17ff5a079fe1201ef2b9955c2f0ce49afc82fc
borkydev <175458672+borkydev@users.noreply.github.com>
2024-07-23 16:28:12 +0200

    removed extra comments

--- init.lua
+++ init.lua
@@ -412,8 +412,7 @@ require('lazy').setup({
       notify_on_error = false,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
+        -- have a well standardized coding style.
         local disable_filetypes = { c = true, cpp = true }
         return {
           timeout_ms = 500,
