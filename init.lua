4a54b9ac1866d53b51d877be39158b2084b8a3d8
borkydev <175458672+borkydev@users.noreply.github.com>
2024-07-23 10:02:37 +0200

    removed extra comments

--- init.lua
+++ init.lua
@@ -86,7 +86,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
